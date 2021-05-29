bce05b464708e068e3009cbae9fabe557fca5f1b
Enzo Albornoz <enzocoelhoalbornoz@gmail.com>
2021-05-29 19:22:37 -0300

    First line drawn

--- py_igs/application_window.py
+++ py_igs/application_window.py
@@ -1,6 +1,7 @@
 # Import Dependencies
 import gi
 from gi.repository import Gtk
+from cairo import Context
 # Setup Graphic
 gi.require_version("Gtk", "3.0")
 gi.require_foreign("cairo")
@@ -9,9 +10,20 @@ gi.require_foreign("cairo")
 class ApplicationWindow(Gtk.ApplicationWindow):
     # Define Mount Point
     __gtype_name__ = "window-root"
-    # Define Handlers
+    # Define Attributes
     widget_logger = Gtk.Template.Child("widget-logger-content")
     widget_logger_scroll = Gtk.Template.Child("widget-logger-historic")
+    # Define Signals
+    @Gtk.Template.Callback("on-canvas-draw")
+    def on_canvas_draw(self, widget: Gtk, ctx: Context):
+        viewport = widget.get_allocation()
+        print(f"[{viewport.width}x{viewport.height}] at [{viewport.x},{viewport.y}]")
+        # print(rec)
+        ctx.set_source_rgb(1, 1, 1)
+        ctx.set_line_width(2)
+        ctx.move_to(0, 0)
+        ctx.line_to(200, 200)
+        ctx.stroke()
     # Define Utility Functions
     def console_log(self, message):
         # Load Console Buffer
